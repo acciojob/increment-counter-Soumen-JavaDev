731eabe22b7136f6f461a605efeb2d1463835503
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-04-20 09:20:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let counter = document.querySelector("#counter");
 let btn = document.querySelector("#incrementBtn");
 btn.onclick = function() {
 	
-	
+	alert(count)
 	count++;
 	counter.innerHTML = count; 
-	alert(count)
+	
 	 }
